9856dbd187bca8cecb641d2581b4224fd2733ff8
Ryan <ryeean16@gmail.com>
2023-08-04 19:12:55 -0700

    styling finished for upload page

--- src/components/Upload/Upload.jsx
+++ src/components/Upload/Upload.jsx
@@ -1,43 +1,60 @@
-import './Upload.scss';
-import { Link } from 'react-router-dom';
-import thumbnail from '../../assets/Images/Upload-video-preview.jpg'
+import "./Upload.scss";
+import { Link } from "react-router-dom";
+import thumbnail from "../../assets/Images/Upload-video-preview.jpg";
 
-function Upload () {
-    return (
-        <>
-        <section className="upload">
+function Upload() {
+  return (
+    <>
+    <div className="upload__twoContainers">
+      <section className="upload">
         <div className="upload__container">
-            <div className="upload__title">
-                Upload Video
-            </div>
-            <div className="upload__thumbnailTitle">
-                VIDEO THUMBNAIL
-            </div>
-            <div className="upload__thumbnail">
-                <video className="upload__video--poster" src='' poster={thumbnail}></video>
-            </div>
+          <div className="upload__title">Upload Video</div>
+          <div className="upload__thumbnailTitle">VIDEO THUMBNAIL</div>
+          <div className="upload__thumbnail">
+            <video
+              className="upload__video--poster"
+              src=""
+              poster={thumbnail}
+            ></video>
+          </div>
         </div>
-        </section>
+      </section>
 
-        <section className="uploadText">
-            <div className="uploadText__container">
-                <div className="uploadText__title uploadText__title1">TITLE YOUR VIDEO</div>
-                <input type="text" placeholder='Add a title to your video' className="uploadText__input" />
-                <div className="uploadText__title">ADD A VIDEO DESCRIPTION</div>
-                <textarea name="" id="" cols="30" rows="10" placeholder='Add a description to your video' className="uploadText__textarea"></textarea>
-                
-            </div>
-            <div className="uploadButton">
-                <div className="uploadButton__publish">
-                    <button className='uploadButton__publishBtn'>PUBLISH</button>
-                </div>
-                <div className="uploadButton__cancel">
-                    <Link to="/"><button className="uploadButton__cancelBtn">CANCEL</button></Link>
-                </div>
-            </div>
-        </section>
-        </>
-    )
+      <section className="uploadText">
+        <div className="uploadText__container">
+          <div className="uploadText__title uploadText__title1">
+            TITLE YOUR VIDEO
+          </div>
+          <input
+            type="text"
+            placeholder="Add a title to your video"
+            className="uploadText__input"
+          />
+          <div className="uploadText__title">ADD A VIDEO DESCRIPTION</div>
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Add a description to your video"
+            className="uploadText__textarea"
+          ></textarea>
+        </div>
+      </section>
+    </div>
+
+      <section className="uploadButton">
+        <div className="uploadButton__publish">
+          <button className="uploadButton__publishBtn">PUBLISH</button>
+        </div>
+        <div className="uploadButton__cancel">
+          <Link to="/">
+            <button className="uploadButton__cancelBtn">CANCEL</button>
+          </Link>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default Upload
+export default Upload;
--- src/components/Videos/Videos.jsx
+++ src/components/Videos/Videos.jsx
@@ -1,16 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Videos.scss';
-import videoDetails from '../../assets/Data/video-details.json';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
+const api_key = '?api_key=b500e183-5e41-4884-ad8e-f7190a88b1c8';
 
+function Video({ id, title, channel, image }) {
 
-
-
-
-function Video({ id, title, channel, image, clickHandler }) {
-    return (
-      <Link to={`/videos/${id}`}>
+  return (
+    <Link to={`/videos/${id}`}>
       <div className="videos__row">
         <img src={image} alt="thumbnail" className="videos__image" />
         <div className="videos__info">
@@ -18,36 +16,53 @@ function Video({ id, title, channel, image, clickHandler }) {
           <div className="videos__channel">{channel}</div>
         </div>
       </div>
-      </Link>
-    );
+    </Link>
+  );
+}
+
+function Videos({ selectedVideo }) {
+  const [videoList, setVideoList] = useState([]);
+
+  useEffect(() => {
+    getVideoData();
+  }, [selectedVideo]);
+  
+
+  function getVideoData() {
+    axios
+    .get(`https://project-2-api.herokuapp.com/videos/${api_key}`)
+    .then((response) => {
+      setVideoList(response.data);
+    })
+    .catch((error) => {
+      console.error('Error fetching video data:', error);
+    });
   }
 
-
-  
-  function Videos({selectedVideo, clickHandler}) {
-  
-    return (
-      <section className="videos">
-        <div className="videos__container">
-          <div className="videos__header">NEXT VIDEOS</div>
-          <div className="videos__card">
-            {videoDetails?.filter(video => selectedVideo.id !== video.id)?.map((video) => 
+  return (
+    <section className="videos">
+      <div className="videos__container">
+        <div className="videos__header">NEXT VIDEOS</div>
+        <div className="videos__card">
+          {videoList
+            .filter((video) => selectedVideo.id !== video.id)
+            .map((video) => (
               <Video
                 key={video.id}
                 id={video.id}
                 title={video.title}
                 channel={video.channel}
                 image={video.image}
-                // clickHandler={clickHandler}
               />
-            )}
-          </div>
+            ))}
         </div>
-      </section>
-    );
-  }
-  
-  export default Videos;
+      </div>
+    </section>
+  );
+}
+
+export default Videos;
+
   
 
   
